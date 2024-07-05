57b1790492477b99f4c22839ef990ac8099f566f
Bhanu Pratap <piyushkumardeoghar05.com>
2024-07-05 17:29:20 +0530

    dd

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const BASE_URL = process.env.BASE_URL;
+console.log(BASE_URL);
+const corsOptions ={
+  origin:`${BASE_URL}`, 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+app.use(express.json());
+const formRouter = require("./Routes/formRoute");
+const subscriptionRouter = require("./Routes/subscriptionRoute");
+const uri =process.env.URI;
+const PORT = 5000;
+app.use(express.json());
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => console.log(err));
+  app.use("/form", formRouter);
+  app.use("/subscription",subscriptionRouter );
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
